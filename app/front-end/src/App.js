2b4944b5eaf6a1ac3ab1a2a621d5a478d7513656
Joao Gustavo <jgustavomendonca@gmail.com>
2023-05-04 16:16:00 -0300

    feat: :sparkles: Adicionando a estrutura inicial das páginas

--- app/front-end/src/App.js
+++ app/front-end/src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom'
 import './App.css';
+import HomePage from './pages/HomePage/HomePage';
+import AboutPage from './pages/AboutPage/AboutPage';
+import ProductsPage from './pages/ProductsPage/ProductsPage';
+import ContactPage from './pages/ContactPage/ContactPage';
 
 function App() {
   return (
     <div className="App">
-      Iniciando a aplicação.
+      <Switch>
+        <Route exact path="/" component={ HomePage } />
+        <Route exact path="/quem-somos" component={ AboutPage } />
+        <Route exact path="/produtos-kanguruh" component={ ProductsPage } />
+        <Route exact path="/contato" component={ ContactPage } />
+      </Switch>
     </div>
   );
 }
