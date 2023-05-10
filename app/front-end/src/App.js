f6cd44a4b243254197dfa38b45f2753b54c4c394
Joao Gustavo <jgustavomendonca@gmail.com>
2023-05-10 10:37:41 -0300

    refactor: :art: Criação de um arquivo routes para as rotas do front e do Context

--- app/front-end/src/App.js
+++ app/front-end/src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom'
+import Routes from './routes/Routes';
 import './App.css';
-import HomePage from './pages/HomePage/HomePage';
-import AboutPage from './pages/AboutPage/AboutPage';
-import ProductsPage from './pages/ProductsPage/ProductsPage';
-import ContactPage from './pages/ContactPage/ContactPage';
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/" component={ HomePage } />
-        <Route exact path="/quem-somos" component={ AboutPage } />
-        <Route exact path="/produtos-kanguruh" component={ ProductsPage } />
-        <Route exact path="/contato" component={ ContactPage } />
-      </Switch>
+      <Routes />
     </div>
   );
 }
---
+++ app/front-end/src/routes/Routes.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from '../pages/HomePage/HomePage';
+import AboutPage from '../pages/AboutPage/AboutPage';
+import ProductsPage from '../pages/ProductsPage/ProductsPage';
+import ContactPage from '../pages/ContactPage/ContactPage';
+
+function Routes() {
+  return (
+    <Switch>
+        <Route exact path="/" component={ HomePage } />
+        <Route exact path="/quem-somos" component={ AboutPage } />
+        <Route exact path="/produtos-kanguruh" component={ ProductsPage } />
+        <Route exact path="/contato" component={ ContactPage } />
+      </Switch>
+  )
+}
+
+export default Routes
