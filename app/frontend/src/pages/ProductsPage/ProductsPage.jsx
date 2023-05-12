8bbcc1478178d5a5d482458747b74c6c2a09d1f2
Joao Gustavo <jgustavomendonca@gmail.com>
2023-05-12 16:49:27 -0300

    feat: :construction: Adicionando a estrutura inicial dos cards

--- app/frontend/src/pages/ProductsPage/ProductsPage.jsx
+++ app/frontend/src/pages/ProductsPage/ProductsPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import Card from '../../components/Card/Card'
 
 function ProductsPage() {
   return (
     <main>
       <Header />
       <div>ProductsPage</div>
+      <Card />
       <Footer />
     </main>
   )
